ee189ef1ee4a48938b9b883b573815e3c5b83167
qimanya <qimanya88@gmail.com>
2025-05-22 12:42:31 +0800

    chore: 替换为新的 firebaseConfig 配置

--- src/components/HomeClient.js
+++ src/components/HomeClient.js
@@ -3,14 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set, update, remove, push, serverTimestamp } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyD-TNMERMJYIdHFGJRnueHoWKF3vvQjfpQ",
+  authDomain: "ojbk-2fb8c.firebaseapp.com",
+  projectId: "ojbk-2fb8c",
+  storageBucket: "ojbk-2fb8c.firebasestorage.app",
+  messagingSenderId: "831587459313",
+  appId: "1:831587459313:web:8c3211ece2326606108950"
 };
 
 const AVAILABLE_ROLES = ['Student', 'Teacher', 'Guard'];
